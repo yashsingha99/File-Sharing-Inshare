bbd0f33f8fcdf85db85cd6188726bb59a77066d1
Yash Singhal <singhaly914@gmail.com>
2024-06-24 17:06:28 +0530

    add createPlan

---
+++ backend/src/controllers/plan.controllers.js
@@ -0,0 +1,26 @@
+const User = require("../Model/user.model");
+const File = require("../Model/file.model")
+const Plan = require("../Model/plan.model")
+
+const createPlan = async (req, res) => {
+try {
+        const validValue = req?.Validity ? req.Validity : 1;
+        const filevalue   = req?.file ? req.file : 2;
+        const dataValue  = req?.data ? req.data : 2;
+        const rupees     =   req?.data ? req.data : 0;
+        const newPlan = await Plan.create({
+            validValue,
+            filevalue ,
+            dataValue,
+            rupees    
+        })
+    
+        if (!newPlan) res.status(400).json({ message: "Internal issue" });
+    
+        res
+          .status(200)
+          .json({ newPlan, message: "successfully created new plan" });
+} catch (error) {
+    console.log("createPlan", error);
+}
+}
