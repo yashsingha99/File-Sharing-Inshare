d3d419d2803960b3ab10228f7521f4e397f0c0e6
Yash Singhal <singhaly914@gmail.com>
2024-06-28 22:56:24 +0530

    add changeisActivate

--- backend/src/routers/user.routes.js
+++ backend/src/routers/user.routes.js
@@ -1,5 +1,5 @@
 const {Router} = require('express')
-const {register, login, addPlan, fetchPurchashedPlans, fetchPlan, updateUser} = require('../controllers/user.controller')
+const {register, changeisActivate, login, addPlan, fetchPurchashedPlans, fetchPlan, updateUser} = require('../controllers/user.controller')
 const router = Router();
 
 
@@ -10,6 +10,7 @@ router.post('/addPlan', addPlan)
 router.get('/fetchPlan', fetchPlan)
 router.post('/updateUser', updateUser)
 router.post('/fetchPurchashedPlans', fetchPurchashedPlans)
+router.post('/changeisActivate', changeisActivate)
 
 
 module.exports = router
